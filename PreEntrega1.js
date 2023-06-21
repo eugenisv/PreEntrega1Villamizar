2cc4bd8704f27b9115c09f01738e6cf70f05ee84
genis <comp.genis@gmail.com>
2023-06-20 22:44:54 -0400

    Código actualizado para soportar objetos

--- PreEntrega1.js
+++ PreEntrega1.js
@@ -1,86 +1,105 @@
 let nombre = prompt("Ingrese su nombre")
 let opciones = "1. Ver carrito \n2. Añadir artículos \n3. Eliminar artículo \n4. Generar Factura\n5. Cerrar"
 var carritoItems = []
-var carritoPrecios = []
-let opcionesItems = ["Ensalada de Quinoa y Aguacate", "Tacos de Coliflor", "Bowl de Arroz Integral y Lentejas", "Tostadas de Frijoles", "Wrap de Hummus y Verduras", "Hamburguesa de Lentejas"]
-let precioItems = [10.99, 3.50, 5.55, 1.95, 4.50, 9.95]
 
+class Producto {
+    constructor (nombreProducto, precioProducto, descripcionProducto) {
+        this.nombreProducto = nombreProducto;
+        this.precioProducto = precioProducto;
+        this.descripcionProducto = descripcionProducto;
+    }
+
+}
+
+const producto1 = new Producto ("Ensalada de Quinoa y Aguacate", 10.99, "Quinoa, aguacate, tomate, cebolla, cilantro y vinagreta de limón.")
+
+const producto2 = new Producto ("Tacos de Coliflor", 3.50, "Tortillas de maíz rellenas de coliflor asada, cebolla y cilantro, con guacamole y salsa de chile.")
+
+const producto3 = new Producto ("Bowl de Arroz Integral y Lentejas", 5.55, "Arroz integral, lentejas, espinacas, pepino, zanahoria, tomate y aderezo de tahini.")
+
+const producto4 = new Producto ("Tostadas de Frijoles", 1.95, "Tostadas crujientes con frijoles negros, aguacate, tomate, cilantro y salsa de chile.")
+
+const producto5 = new Producto ("Wrap de Hummus y Verduras", 4.50, "Tortilla de avena rellena de hummus, espinacas, pepino, zanahoria, tomate y cebolla roja.")
+
+const producto6 = new Producto ("Hamburguesa de Lentejas", 9.95, "Hamburguesa vegetariana de lentejas con aguacate, tomate, cebolla y aderezo de tahini.")
+
+const listaProductos = [producto1, producto2, producto3, producto4, producto5, producto6]
 
 while (true) {
     var opcion = prompt(`Hola, ${nombre}. \nPor favor, seleccione una opción indicando un número.\n` + opciones)
 
     switch (opcion) {
         case "1":
-            VerCarrito(carritoItems, carritoPrecios);
+            VerCarrito(carritoItems);
             break;
         case "2":
-            añadirArticulo(opcionesItems, precioItems, carritoItems, carritoPrecios);
+            añadirArticulo(listaProductos, carritoItems);
             break;
         case "3":
-            eliminarArticulo(carritoItems, carritoPrecios);
+            eliminarArticulo(carritoItems);
             break;
         case "4":
-            facturar(carritoItems, carritoPrecios, nombre);
+            facturar(carritoItems, nombre);
             break;
-         case "5":
-            alert("Hasta luego!");
+
+        case "5":
+            console.log("Hasta luego!");
             break;
         default:
-            alert("Por favor, selecciona una de las opciones")
+            console.log("Por favor, selecciona una de las opciones")
     }
 }
 
-function VerCarrito (nombres, precios) {
+
+function VerCarrito (carrito) {
     compras = ""
-    if (nombres.length < 1) {
+    if (carrito.length < 1) {
         alert("No tienes nada agregado a tu carrito")
     }
     else {
-        for (i=0; i < nombres.length; i++) {
-            compras = compras + `${i+1}. ${nombres[i]}, $${precios[i]}.\n`
+        for (i=0; i < carrito.length; i++) {
+            compras = compras + `${i+1}. ${carrito[i].nombreProducto}, $${carrito[i].precioProducto}.\n`
         }
         alert(`Estos son los items que tienes añadidos:\n${compras}`)
     }
     
 }
 
-function addArticulo (n, nombres, precios, carrito, costos) {
-    carrito.push(nombres[n])
-    costos.push(precios[n])
+function addArticulo (n, productos, carrito) {
+    carrito.push(productos[n])
 }
 
-function delArticulo (n, carrito, costos) {
+function delArticulo (n, carrito) {
     carrito.splice(n, 1)
-    costos.splice(n, 1)
 }
 
-function añadirArticulo (nombres, precios, carrito, costos) {
+function añadirArticulo (productos, carrito) {
     mensaje = "Seleccione el artículo a añadir \n"
-    for (i=0; i < nombres.length; i++) {
-        mensaje = mensaje + `${i+1}. ${nombres[i]}, $${precios[i]}.\n`
+    for (i=0; i < productos.length; i++) {
+        mensaje = mensaje + `${i+1}. ${productos[i].nombreProducto}, $${productos[i].precioProducto}.\n ${productos[i].descripcionProducto}\n`
         console.log(i)
     }
     n = parseInt(prompt(mensaje)) - 1 
-    addArticulo(n, nombres, precios, carrito, costos)
+    addArticulo(n, productos, carrito)
 }
 
-function eliminarArticulo (carrito, costos) {
+function eliminarArticulo (carrito) {
     mensaje = "Seleccione el artículo a eliminar \n"
     for (i=0; i < carrito.length; i++) {
-        mensaje = mensaje + `${i+1}. ${carrito[i]}, $${costos[i]}.\n`
+        mensaje = mensaje + `${i+1}. ${carrito[i].nombreProducto}, $${carrito[i].precioProducto}.\n`
     }
     n = parseInt(prompt(mensaje)) - 1 
-    delArticulo(n, carrito, costos)
+    delArticulo(n, carrito)
 }
 
-function facturar (nombres, precios, nombre) {
+function facturar (carrito, nombre) {
     compras = ""
     total = 0
-    for (i=0; i < nombres.length; i++) {
-        compras = compras + `${i+1}. ${nombres[i]}, $${precios[i]}.\n`
-        total = total + precios[i]
+    for (i=0; i < carrito.length; i++) {
+        compras = compras + `${i+1}. ${carrito[i].nombreProducto}, $${carrito[i].precioProducto}.\n`
+        total = total + carrito[i].precioProducto
     }
-    alert(`${nombre}, Estos son los items que ordenaste:\n${compras}\n El total a pagar es $${total}\nEl pedido ha sido generado exitosamente, visítanos para retirar lo que ordenaste.`)
+    alert(`${nombre}, Estos son los items que ordenaste:\n${compras}\nEl total a pagar es $${total.toFixed(2)}\nEl pedido ha sido generado exitosamente, visítanos para retirar lo que ordenaste.`)
 }
 
 
